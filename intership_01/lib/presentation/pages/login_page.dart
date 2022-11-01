4c39f7a496b35bbba733ce4b233e36c3e6c24a9d
Handika Rafli A <shandika231@gmail.com>
2022-11-01 16:25:08 +0700

    refactor login widget

--- intership_01/lib/presentation/pages/login_page.dart
+++ intership_01/lib/presentation/pages/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intership_01/common/extension.dart';
 import 'package:intership_01/common/style.dart';
 
-import '../widgets/item_text_field_widget.dart';
+import '../widgets/login_widget.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -15,57 +15,21 @@ class LoginPage extends StatelessWidget {
           Expanded(
             flex: 4,
             child: Container(
-              color: Colors.orange,
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: blackColor,
+              color: whiteColor,
               child: Center(
-                child: DefaultTextStyle(
-                  style: const TextStyle(
-                    fontSize: 16,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'logo'.path,
-                        width: 110,
-                        height: 110,
-                        fit: BoxFit.cover,
-                      ),
-                      const SizedBox(
-                        height: 60,
-                      ),
-                      const ItemTextFieldWidget(
-                        hintText: 'Username',
-                        text: 'Username',
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const ItemTextFieldWidget(
-                        hintText: 'Password',
-                        text: 'Password',
-                        isPassword: true,
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Sign In",
-                        ),
-                      ),
-                    ],
-                  ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'bg-login'.path,
+                      width: 500,
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
+          const LoginWidget(),
         ],
       ),
     );
