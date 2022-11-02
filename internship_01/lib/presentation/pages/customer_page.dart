3774c1dc15c11396075cd4ca9ae633bb9b08f131
Handika Rafli A <shandika231@gmail.com>
2022-11-02 09:46:04 +0700

    refactor customer page

--- internship_01/lib/presentation/pages/customer_page.dart
+++ internship_01/lib/presentation/pages/customer_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intership_01/common/extension.dart';
 import 'package:intership_01/common/style.dart';
-import 'package:intership_01/domain/model/customer_model.dart';
 
 import '../widgets/build_drawer_widget.dart';
+import '../widgets/heading_customer_widget.dart';
+import '../widgets/table_customer_widget.dart';
 
 class CustomerPage extends StatelessWidget {
   static const nameRoute = '/customer';
@@ -65,156 +66,9 @@ class CustomerPage extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Data Nasabah',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      labelText: 'Telusuri',
-                      suffixIcon: const Icon(Icons.search),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  headingTextStyle: TextStyle(
-                    fontSize: 15,
-                    color: greyColor,
-                  ),
-                  dataTextStyle: TextStyle(
-                    fontSize: 15,
-                    fontWeight: bold,
-                    color: blackColor,
-                  ),
-                  border: TableBorder(
-                    horizontalInside: BorderSide(
-                      color: greyColor,
-                      width: 1,
-                    ),
-                  ),
-                  columns: const [
-                    DataColumn(
-                      label: Text('CIF'),
-                    ),
-                    DataColumn(
-                      label: Text('Nama'),
-                    ),
-                    DataColumn(
-                      label: Text('NIK'),
-                    ),
-                    DataColumn(
-                      label: Text('Nomor Telepon'),
-                    ),
-                    DataColumn(
-                      label: Text('Alamat'),
-                    ),
-                    DataColumn(
-                      label: Text('Pekerjaan'),
-                    ),
-                    DataColumn(
-                      label: Text('Email'),
-                    ),
-                    DataColumn(
-                      label: Text('Status'),
-                    ),
-                    DataColumn(
-                      label: Text(''),
-                    ),
-                  ],
-                  rows: dataCustomer
-                      .map(
-                        (customer) => DataRow(
-                          cells: [
-                            DataCell(
-                              Text(
-                                customer.id,
-                                style: TextStyle(
-                                  color: greenColor,
-                                ),
-                              ),
-                            ),
-                            DataCell(
-                              Text(customer.name),
-                            ),
-                            DataCell(
-                              Text(
-                                customer.nik,
-                                style: TextStyle(
-                                  fontWeight: regular,
-                                ),
-                              ),
-                            ),
-                            DataCell(
-                              Text(
-                                customer.phone,
-                                style: TextStyle(
-                                  fontWeight: regular,
-                                ),
-                              ),
-                            ),
-                            DataCell(
-                              Text(customer.address),
-                            ),
-                            DataCell(
-                              Text(customer.job),
-                            ),
-                            DataCell(
-                              Text(customer.email),
-                            ),
-                            DataCell(
-                              Text(
-                                customer.status ? 'Aktif' : 'Backlist',
-                                style: TextStyle(
-                                  color:
-                                      customer.status ? greenColor : redColor,
-                                ),
-                              ),
-                            ),
-                            DataCell(
-                              Material(
-                                color: orangeColor,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: InkWell(
-                                  borderRadius: BorderRadius.circular(8),
-                                  onTap: customer.onTap,
-                                  child: Container(
-                                    padding: const EdgeInsets.all(8),
-                                    child: Icon(
-                                      Icons.edit,
-                                      size: 16,
-                                      color: whiteColor,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-            ),
+          children: const [
+            HeadingCustomerWidget(),
+            TableCustomerWidget(),
           ],
         ),
       ),
---
+++ internship_01/lib/presentation/widgets/table_customer_widget.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+import '../../common/style.dart';
+import '../../domain/model/customer_model.dart';
+
+class TableCustomerWidget extends StatelessWidget {
+  const TableCustomerWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          headingTextStyle: TextStyle(
+            fontSize: 15,
+            color: greyColor,
+          ),
+          dataTextStyle: TextStyle(
+            fontSize: 15,
+            fontWeight: bold,
+            color: blackColor,
+          ),
+          border: TableBorder(
+            horizontalInside: BorderSide(
+              color: greyColor,
+              width: 1,
+            ),
+          ),
+          columns: const [
+            DataColumn(
+              label: Text('CIF'),
+            ),
+            DataColumn(
+              label: Text('Nama'),
+            ),
+            DataColumn(
+              label: Text('NIK'),
+            ),
+            DataColumn(
+              label: Text('Nomor Telepon'),
+            ),
+            DataColumn(
+              label: Text('Alamat'),
+            ),
+            DataColumn(
+              label: Text('Pekerjaan'),
+            ),
+            DataColumn(
+              label: Text('Email'),
+            ),
+            DataColumn(
+              label: Text('Status'),
+            ),
+            DataColumn(
+              label: Text(''),
+            ),
+          ],
+          rows: dataCustomer
+              .map(
+                (customer) => DataRow(
+                  cells: [
+                    DataCell(
+                      Text(
+                        customer.id,
+                        style: TextStyle(
+                          color: greenColor,
+                        ),
+                      ),
+                    ),
+                    DataCell(
+                      Text(customer.name),
+                    ),
+                    DataCell(
+                      Text(
+                        customer.nik,
+                        style: TextStyle(
+                          fontWeight: regular,
+                        ),
+                      ),
+                    ),
+                    DataCell(
+                      Text(
+                        customer.phone,
+                        style: TextStyle(
+                          fontWeight: regular,
+                        ),
+                      ),
+                    ),
+                    DataCell(
+                      Text(customer.address),
+                    ),
+                    DataCell(
+                      Text(customer.job),
+                    ),
+                    DataCell(
+                      Text(customer.email),
+                    ),
+                    DataCell(
+                      Text(
+                        customer.status ? 'Aktif' : 'Backlist',
+                        style: TextStyle(
+                          color: customer.status ? greenColor : redColor,
+                        ),
+                      ),
+                    ),
+                    DataCell(
+                      Material(
+                        color: orangeColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(8),
+                          onTap: customer.onTap,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            child: Icon(
+                              Icons.edit,
+                              size: 16,
+                              color: whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
